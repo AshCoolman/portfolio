c09c09590a356b83e862eafd6904cfa529a4135e
AshCoolman <writetofish@gmail.com>
2013-11-08 02:40:44 +0000

    Makes deployable version

--- buildv3/index.php
+++ buildv3/index.php
@@ -2,7 +2,7 @@
 <!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
 <?php
-$IS_DEPLOY = false;
+$IS_DEPLOY = true;
 ?>
 <head>
 	<!--link href='http://fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'-->
